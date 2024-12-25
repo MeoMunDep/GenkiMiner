e412d3c76951f37579683917239964238280f19f
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2024-12-26 04:11:17 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,16 +30,16 @@ fi
 create_default_configs() {
     cat > configs.json << EOL
 {
-    "timeZone": "en-US",
-    "rotateProxy": false,
-    "skipInvalidProxy": false,
-    "proxyRotationInterval": 2,
-    "delayEachAccount": [5, 8],
-    "timeToRestartAllAccounts": 300,
-    "howManyAccountsRunInOneTime": 10,
-    "doTasks": true,
-    "playGames": true,
-    "referralCode": ""
+  "timeZone": "en-US",
+  "isSkipInvalidProxy": false,
+  "delayEachAccount": [5, 8],
+  "timeToRestartAllAccounts": 300,
+  "howManyAccountsRunInOneTime": 100,
+  "isDoTasks": false,
+  "isPlayGames": true,
+  "referralCode": "",
+  "amountOfPickAxeUpgrade": 5,
+  "watchAds": true
 }
 EOL
 }
@@ -55,7 +55,7 @@ check_configs() {
 while true; do
     clear
     echo "============================================================================"
-    echo "    name BOT SETUP AND RUN SCRIPT"
+    echo "    Genkiminer BOT SETUP AND RUN SCRIPT"
     echo "============================================================================"
     echo
     echo "Current directory: $(pwd)"
@@ -115,7 +115,7 @@ while true; do
             else
                 print_green "Using node_modules from current directory"
             fi
-            node bot
+            cd Genkiminer && node bot
             read -p "Press Enter to continue..."
             ;;
         4)
