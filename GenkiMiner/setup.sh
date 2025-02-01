d870cb7a2ebb9c551dae87bc56a421ff74f62fb6
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-02-02 00:07:23 +0700

    fix error...

--- GenkiMiner/setup.sh
+++ GenkiMiner/setup.sh
@@ -55,7 +55,7 @@ check_configs() {
 while true; do
     clear
     echo "============================================================================"
-    echo "    Genkiminer BOT SETUP AND RUN SCRIPT"
+    echo "    Genkiminer BOT SETUP AND RUN SCRIPT by @MeoMunDep"
     echo "============================================================================"
     echo
     echo "Current directory: $(pwd)"
@@ -115,7 +115,7 @@ while true; do
             else
                 print_green "Using node_modules from current directory"
             fi
-            cd Genkiminer && node bot
+            node meomundep
             read -p "Press Enter to continue..."
             ;;
         4)
