70253b6d1a10be181a6da79a2b05b853b269227a
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-03-17 00:37:00 +0700

    Create run.bat

---
+++ GenkiMiner/run.bat
@@ -0,0 +1,34 @@
+@echo off
+title Genkiminer Bot by @MeoMunDep
+color 0A
+
+cd %~dp0
+
+echo Checking configuration files...
+
+(for %%F in (datas.txt proxies.txt) do (
+    if not exist %%F (
+        type nul > %%F
+        echo Created %%F
+    )
+))
+
+echo Configuration files checked.
+
+echo Checking dependencies...
+if exist "..\node_modules" (
+    echo Using node_modules from parent directory...
+    cd ..
+    CALL npm install user-agents axios colors https-proxy-agent socks-proxy-agent pako uuid crypto-js
+    cd %~dp0
+) else (
+    echo Installing dependencies in current directory...
+    CALL npm install user-agents axios colors https-proxy-agent socks-proxy-agent pako uuid crypto-js
+)
+echo Dependencies installation completed!
+
+echo Starting the bot...
+node meomundep
+
+pause
+exit
